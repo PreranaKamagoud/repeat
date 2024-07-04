4794604f0c389dde299eeedf07671eb54736520d
prerana <Preranakamagoud@gmail.com>
2024-07-04 14:52:04 +0530

    I have uploaded project check

---
+++ inspire.py
@@ -0,0 +1,5 @@
+print(f"Silence is a best answer for all stupid questions")
+print(f"Smile is the best answer for all critical situations...")
+print(f"Trust in shiva,shiva always bless you...")
+print(f"Being alone is best in life rather than communicating with anothers..")
+
